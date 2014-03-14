6164cea433e02109193472801b83541bfd837524
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2014-03-14 19:01:57 +0100

    Toggle node on label click

--- views/default/js/framework/categories/tree.php
+++ views/default/js/framework/categories/tree.php
@@ -7,6 +7,9 @@
 			e.preventDefault();
 			$(this).closest('li').toggleClass('elgg-menu-open elgg-menu-closed');
 		});
+		$('.elgg-child-menu-toggle + label').live('click', function(e) {
+			$(this).siblings('.elgg-child-menu-toggle').trigger('click');
+		});
 	};
 
 	elgg.register_hook_handler('init', 'system', framework.categories.input);
