30497847a374005e3b8c43d1e76860c11307f90e
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2015-01-10 14:28:41 +0100

    Update activation script

--- activate.php
+++ activate.php
@@ -1,13 +1,15 @@
 <?php
 
+namespace hypeJunction\Categories;
+
+require_once __DIR__ . '/vendors/autoload.php';
+
 $subtypes = array(
-	'hjcategory' => 'hypeJunction\\Categories\\Category',
+	'hjcategory' => get_class(new Category),
 );
 
 foreach ($subtypes as $subtype => $class) {
-	if (get_subtype_id('object', $subtype)) {
-		update_subtype('object', $subtype, $class);
-	} else {
+	if (!update_subtype('object', $subtype, $class)) {
 		add_subtype('object', $subtype, $class);
 	}
 }
